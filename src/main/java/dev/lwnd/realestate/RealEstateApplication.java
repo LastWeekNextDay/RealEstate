ef3b10d619673fad5e084227387642c6554ffce8
LastWeekNextDay <doesn@mail.ru>
2024-04-02 23:15:40 +0300

    Google API in args
    Added ability to provide google API key in args

--- src/main/java/dev/lwnd/realestate/RealEstateApplication.java
+++ src/main/java/dev/lwnd/realestate/RealEstateApplication.java
@@ -7,8 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealEstateApplication {
 
     public static void main(String[] args) {
-        if (Global.GOOGLE_API_KEY.isEmpty())
-            throw new RuntimeException("Please set your Google API Key in Global.GOOGLE_API_KEY");
+        if (Global.GOOGLE_API_KEY.isEmpty()){
+            if (args.length == 0)
+                throw new RuntimeException("Please set your Google API Key in Global.GOOGLE_API_KEY or pass it as an argument.");
+            else
+                Global.GOOGLE_API_KEY = args[0];
+        }
+
         SpringApplication.run(RealEstateApplication.class, args);
     }
 
